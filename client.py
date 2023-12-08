c07452fb241591be7dc1084f8d5ae19fe9e03ac1
Bodan07 <najmi.kamil@gmail.com>
2023-12-08 16:17:07 +0700

    menu

--- client.py
+++ client.py
@@ -24,13 +24,37 @@ def send_notification(boarding_schedule, transit_location):
     print("Meminta data boarding . . .")
 
     
+# Dictionary jadwal dan lokasi
+list_jadwal = {
+    "2023-12-02 12:30:00": "Bandung",
+    "2023-12-02 14:30:00": "Yogyakarta",
+    "2023-12-02 15:30:00": "Bali",
+    "2023-12-02 16:30:00": "Lombok",
+    "2023-12-02 18:30:00": "Labuan_Bajo"
+}
 
+# Menu untuk memilih jadwal dan lokasi
+while True:
+    print("\nMenu Pilihan Jadwal dan Lokasi:")
+    for i, (jadwal, lokasi) in enumerate(list_jadwal.items(), 1):
+        print(f"{i}. Jadwal: {jadwal}, Lokasi: {lokasi}")
 
-# Simulasi pengiriman pesan
-send_notification("2023-12-02 15:30:00", "Bali")
+    print("0. Keluar")
+    user_input = input("Pilih nomor jadwal (0 untuk keluar): ")
 
-# Tunggu beberapa detik
-time.sleep(15)
+    if user_input == "0":
+        break
+    elif user_input.isdigit():
+        index = int(user_input)
+        if 1 <= index <= len(list_jadwal):
+            selected_schedule = list(list_jadwal.keys())[index - 1]
+            selected_location = list(list_jadwal.values())[index - 1]
 
-# Simulasi pengiriman pesan baru
-send_notification("2023-12-02 12:30:00", "Yogyakarta")
+            # Simulasi pengiriman pesan
+            send_notification(selected_schedule, selected_location)
+            # Tunggu beberapa detik
+            time.sleep(15)
+        else:
+            print("Nomor jadwal tidak valid. Silakan pilih nomor yang sesuai.")
+    else:
+        print("Input tidak valid. Masukkan nomor yang sesuai.")
